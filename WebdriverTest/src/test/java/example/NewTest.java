47a0ac07cae5d6285ce9e8259c779e4d3e57f22f
gaura <gaura@LAPTOP-SJ8DN68H>
2019-06-27 14:08:57 +0530

    set

---
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,29 @@
+package example;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;		
+		
+	    @BeforeTest
+		public void beforeTest() {	
+	    	System.setProperty("webdriver.chrome.driver", "C:\\selenium\\vmock\\src\\test\\resources\\Drivers\\chromedriver.exe");
+	    	driver = new ChromeDriver();
+		}
+	    @Test				
+		public void testEasy() {	
+			driver.get("http://google.com");  
+			
+		}	
+			
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}
