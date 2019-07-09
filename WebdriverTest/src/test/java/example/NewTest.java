58ffdf8c8f70228f0328d9eac5a560b292137ffb
gaura <gaura@LAPTOP-SJ8DN68H>
2019-07-09 15:22:10 +0530

    hguy

--- WebdriverTest/src/test/java/example/NewTest.java
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -24,9 +24,9 @@ public class NewTest {
 	    @BeforeTest
 		public void beforeTest() {	
 	        
-	    	//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+	    	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 	    	//System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
-	    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\gaura\\git\\repository2\\WebdriverTest\\Driver\\chromedriver.exe");
+	    	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\gaura\\git\\repository2\\WebdriverTest\\Driver\\chromedriver.exe");
 	    	//driver = new ChromeDriver();
 	    	ChromeOptions options = new ChromeOptions();  
 	    	options.addArguments("--headless", "--disable-gpu", "--ignore-certificate-errors");
